334f67221e5db9295a5ea36744622f58041bc1e7
TuffenDuffen <50796186+TuffenDuffen@users.noreply.github.com>
2026-09-25 16:17:25 +0000

    initialized project

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use clap::{Parser, Subcommand};
+
+mod server;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Start the GitHub Webhook receiving server
+    Serve,
+}
+
+#[tokio::main]
+async fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Serve) => server::run_server().await,
+        None => todo!(),
+    }
+}
---
+++ src/server.rs
@@ -0,0 +1,12 @@
+use axum::{Router, routing::get};
+
+pub async fn run_server() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
